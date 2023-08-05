77301791db11e83c6dfd354943efc1056eea5a32
apurva95 <apurvatandon@gmail.com>
2023-08-05 22:05:20 +0100

    FinalChanges

--- src/RegistrationForm.tsx
+++ src/RegistrationForm.tsx
@@ -23,7 +23,7 @@ const RegistrationForm: React.FC = () => {
 //https://loggerregistration20230707150626.azurewebsites.net/registration/
     try {
       const response = await axios.post(
-        "https://ec2-3-9-132-176.eu-west-2.compute.amazonaws.com/registration/",
+        "https://localhost:7155/registration/",
         {
           companyName,
           projectName,
